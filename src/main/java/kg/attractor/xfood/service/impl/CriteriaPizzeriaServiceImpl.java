d39e097e79337d28ea4eb3040dc6a7c5b5307c7f
Vladislav K <barabanabanan@gmail.com>
2024-06-03 20:26:44 +0600

    #7   Interfaces and Implementation classes have been created

---
+++ src/main/java/kg/attractor/xfood/service/impl/CriteriaPizzeriaServiceImpl.java
@@ -0,0 +1,6 @@
+package kg.attractor.xfood.service.impl;
+
+import kg.attractor.xfood.service.CriteriaPizzeriaService;
+
+public class CriteriaPizzeriaServiceImpl implements CriteriaPizzeriaService {
+}
