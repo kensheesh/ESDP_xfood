1e10194e9a134b94288938fdd04ccda0883c404d
Danis Kurenkiev <kurenkievdd@gmail.com>
2024-06-07 16:52:39 +0600

    Added CheckListController (MVC)

---
+++ src/main/java/kg/attractor/xfood/controller/mvc/CheckListController.java
@@ -0,0 +1,85 @@
+package kg.attractor.xfood.controller.mvc;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/checks")
+public class CheckListController {
+
+    // ROLE: SUPERVISOR
+    @GetMapping("/create")
+    public String create () {
+        // TODO назначение проверки
+        return null;
+    }
+
+    // ROLE: SUPERVISOR
+    @PostMapping("/create")
+    public String create (BindingResult result, Model model) {
+        return null;
+        // TODO назначение проверки
+    }
+
+//    // ROLE: ADMIN
+//    @GetMapping ("/{id}/edit")
+//    public String edit (@PathVariable (name="id") Long checkListId, Model model) {
+//        return null;
+//    }
+//
+//    // ROLE: ADMIN
+//    @PostMapping ("/{id}/edit")
+//    public String edit (@PathVariable (name="id") Long checkListId, BindingResult result, Model model) {
+//        return null;
+//    }
+
+    // ROLE: EXPERT
+    @GetMapping ("/{id}/check")
+    public String check (@PathVariable (name="id") Long checkListId, Model model) {
+         /* TODO:
+             Проверка
+        */
+
+        return null;
+    }
+
+    // ROLE: EXPERT
+    @PostMapping ("/{id}/check")
+    public String check (@PathVariable (name="id") Long checkListId, BindingResult result, Model model) {
+        /* TODO:
+            Подтверждение проверки экспертом
+        */
+
+        return null;
+    }
+
+    // ROLE: SUPERVISOR, ADMIN
+    @GetMapping ("/{id}/change")
+    public String changeResult (@PathVariable (name="id") Long checkListId, Model model) {
+        /* TODO:
+            Изменение результата проверки *после
+            подтверждения экспертом
+        */
+
+        return null;
+    }
+
+    // ROLE: SUPERVISOR, ADMIN
+    @PostMapping ("/{id}/change")
+    public String changeResult (@PathVariable (name="id") Long checkListId, BindingResult result, Model model) {
+        /* TODO:
+            Изменение результата проверки *после
+            подтверждения экспертом
+        */
+
+        return null;
+    }
+
+}
