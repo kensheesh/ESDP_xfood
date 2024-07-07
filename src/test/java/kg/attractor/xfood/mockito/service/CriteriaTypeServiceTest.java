5550a37ad17c7e2fa8d20db0fff662705d7cc6f0
Bubunur Jumakanova <jumakanovabibinur@gmail.com>
2024-07-07 19:18:57 +0600

    wrote tests for methods of CriteriaPizzeriaService and CriteriaTypeService

---
+++ src/test/java/kg/attractor/xfood/mockito/service/CriteriaTypeServiceTest.java
@@ -0,0 +1,33 @@
+package kg.attractor.xfood.mockito.service;
+
+import kg.attractor.xfood.model.CriteriaType;
+import kg.attractor.xfood.repository.CriteriaTypeRepository;
+import kg.attractor.xfood.service.CriteriaTypeService;
+import kg.attractor.xfood.service.impl.CriteriaTypeServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.*;
+
+public class CriteriaTypeServiceTest {
+    @InjectMocks
+    private CriteriaTypeServiceImpl criteriaTypeService;
+
+    @Mock
+    private CriteriaTypeRepository criteriaTypeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testSave(){
+        CriteriaType criteriaType = new CriteriaType();
+        when(criteriaTypeRepository.save(criteriaType)).thenReturn(criteriaType);
+        criteriaTypeService.save(criteriaType);
+        verify(criteriaTypeRepository, times(1)).save(criteriaType);
+    }
+}
