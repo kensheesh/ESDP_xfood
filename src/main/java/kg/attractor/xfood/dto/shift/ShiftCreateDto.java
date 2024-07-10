e2b42ff94e2d5698e585aa1c9dec1620a82190ba
Danis Kurenkiev <kurenkievdd@gmail.com>
2024-07-10 20:03:06 +0600

    Created DTOs

---
+++ src/main/java/kg/attractor/xfood/dto/shift/ShiftCreateDto.java
@@ -0,0 +1,24 @@
+package kg.attractor.xfood.dto.shift;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShiftCreateDto {
+    private Long id;
+    @Min(0) @Max(23)
+    private Integer startTimeHour;
+    @Min(0) @Max(59)
+    private Integer startTimeMinute;
+    @Min(0) @Max(23)
+    private Integer endTimeHour;
+    @Min(0) @Max(59)
+    private Integer endTimeMinute;
+}
