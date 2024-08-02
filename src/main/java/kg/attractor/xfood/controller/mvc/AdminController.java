9e4e33671fc0949aebe833a57dfb1d349080d512
Ashimov Argen <retreatpm@icloud.com>
2024-08-02 15:12:38 +0300

    Add preAuthorize

--- src/main/java/kg/attractor/xfood/controller/mvc/AdminController.java
+++ src/main/java/kg/attractor/xfood/controller/mvc/AdminController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("admin")
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
 
     private final UserService userService;
