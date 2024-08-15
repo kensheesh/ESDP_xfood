3ff7cfee87967b92c4d7616edfd8597750695f11
Nursultan Beishekeev <nursultanzynwa@gmail.com>
2024-08-15 13:49:44 +0600

    commit for testing ci/cd for pushes

--- src/test/java/kg/attractor/xfood/mockito/service/AdminServiceTest.java
+++ src/test/java/kg/attractor/xfood/mockito/service/AdminServiceTest.java
@@ -18,6 +18,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+
 class AdminServiceTest {
     @InjectMocks
     private AdminServiceImpl adminService;
