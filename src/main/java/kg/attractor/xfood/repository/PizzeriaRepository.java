50584c1e42796e560fe8786b8cc21bb21cb4e8ee
Dosya <abdk4lyk0v@gmail.com>
2024-08-18 00:09:54 +0600

    xfood-119 - deleted last migration 51 - added page to edit pizzeria values - provided pagination and looks

--- src/main/java/kg/attractor/xfood/repository/PizzeriaRepository.java
+++ src/main/java/kg/attractor/xfood/repository/PizzeriaRepository.java
@@ -2,6 +2,8 @@ package kg.attractor.xfood.repository;
 
 import kg.attractor.xfood.dto.pizzeria.PizzeriaShowDto;
 import kg.attractor.xfood.model.Pizzeria;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,9 +11,14 @@ import java.util.List;
 
 @Repository
 public interface PizzeriaRepository extends JpaRepository<Pizzeria, Long> {
-    List<Pizzeria> findByLocation_IdOrderByNameAsc(Long id);
+	List<Pizzeria> findByLocation_Id(Long id);
 	
 	Pizzeria findByUuidEqualsIgnoreCase(String uuid);
 
     List<PizzeriaShowDto> findByNameContainingIgnoreCase(String query);
+	
+	Page<Pizzeria> findByLocation_Id(Pageable pageable, Long locationId);
+	
+	Page<Pizzeria> findBy(Pageable pageable);
+	
 }
